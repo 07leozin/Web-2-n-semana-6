553895ecdcec1e5c185b3b010d1d10b3e48f66bb
07leozin <leonardoalvesmartins2007@gmail.com>
2025-08-07 19:53:01 -0300

    Delete siteleo directory

--- siteleo/excluir.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-if (!isset($_SESSION['logado'])) { header("Location: login.php"); exit; }
-
-$id = $_GET['id'];
-$conn->query("DELETE FROM bebidas WHERE id=$id");
-header("Location: painel.php");
-exit;
